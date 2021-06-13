96a4ccd7c2eaf4557fd947d726e97a5eab5911f5
patricia-faustino <patricia.faustinobr@gmail.com>
2021-06-13 12:38:46 -0300

    Atividades Unidade 1

---
+++ Unidade1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char caractereQualquer = '@';
+	char stringQualquer[30] = "Patricia";
+	int inteiroQualquer = 22;
+	float floatQualquer = 23.5;
+	double doubleQualquer = 24.55;
+	bool booleanoQualquer = true;
+	//enum : dados enumerados 
+	//void: ausência de valores, inicialmente ocupam 0 bits em memória
+	// pointer: localização de memória
+	long int inteiroMaiorQualquer = 22; // aumenta a capacidade de armazenamento
+	unsigned floatPositivoQualquer = 22.5; // duplica a faixa de valores mas não permite números negativos
+	short int inteiroMenorQualquer = 2; // reduz a capacidade de armazenamento
+	
+	
+	return 0;
+	
+}
---
+++ Unidade1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // #include <{nome da biblioteca}>
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*{tipo da função} {nome da função} ({parametros}) {
+	corpo da função
+	return {retorno da função, sendo do mesmo tipo da função}
+	
+}*/
+int main(int argc, char** argv) {
+	printf("Olá mundo!!");
+	
+	return 0;
+}
+
+
